0dcfec74332fb4f10588164e6ee5135d489359a0
sachin5417 <101930885+sachin5417@users.noreply.github.com>
2022-06-26 17:37:16 +0530

    Made changes as requested

--- src/main/java/com/smallwebapp/entity/Smallapp.java
+++ src/main/java/com/smallwebapp/entity/Smallapp.java
@@ -4,7 +4,7 @@ public class Smallapp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Deepak yadav");
+			System.out.println("Sachin Patil");
 	}
 
 }
